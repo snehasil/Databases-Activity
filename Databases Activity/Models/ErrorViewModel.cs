63c255514b6b4204d6e9cfac5f6a3d8f3bd0529f
Sneha Sil <snehasil413@gmail.com>
2021-06-20 12:31:21 -0500

    created project

---
+++ Databases Activity/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Databases_Activity.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
